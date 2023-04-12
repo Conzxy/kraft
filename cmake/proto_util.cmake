3f818decf7865ed686cdeb1f6a15e9fb7a8352ac
Conzxy <1967933844@qq.com>
2023-04-12 14:57:55 +0800

    MOD: depends make regenerate existed pb codes

--- cmake/proto_util.cmake
+++ cmake/proto_util.cmake
@@ -1,6 +1,5 @@
 function (gen_proto_code)
   cmake_parse_arguments(PARSE_ARGV 0 CONZXY "" "PROTOC_PATH" "FILES")
-  message(STATUS "protoc path = ${CONZXY_PROTOC_PATH}")
   foreach (proto_file_fullpath ${CONZXY_FILES})
     get_filename_component(proto_filename ${proto_file_fullpath} NAME_WE)
     get_filename_component(proto_dir ${proto_file_fullpath} DIRECTORY)
@@ -8,6 +7,7 @@ function (gen_proto_code)
       OUTPUT ${proto_filename}.pb.cc ${proto_filename}.pb.h
       COMMAND ${CONZXY_PROTOC_PATH}
       ARGS --cpp_out . ${proto_file_fullpath} -I${proto_dir}
+      DEPENDS ${proto_file_fullpath}
       VERBATIM
     )
   endforeach ()
