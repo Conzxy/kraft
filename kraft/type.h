60d55fe0128348209cdeb28651dba2ac4c7e61d5
Conzxy <1967933844@qq.com>
2023-04-17 19:34:24 +0800

    NEW: entry to meta

--- kraft/type.h
+++ kraft/type.h
@@ -2,8 +2,8 @@
 #ifndef _KRAFT_TYPE_H__
 #define _KRAFT_TYPE_H__
 
-#include <unordered_map>
 #include <stdint.h>
+#include <unordered_map>
 
 namespace kraft {
 
@@ -14,6 +14,14 @@ using HashMap = std::unordered_map<K, V, Hash, Pred, Alloc>;
 
 using u64 = uint64_t;
 using u8 = uint8_t;
+
+// The metadata describe the log entry
+//
+// NOTE: We don't care the entry type(confchange or normal operation)
+struct EntryMeta {
+  u64 term = -1;
+  u64 index = -1;
+};
 
 } // namespace kraft
 
--- kraft/util.h
+++ kraft/util.h
@@ -1,4 +1,20 @@
 #ifndef _KRAFT_UTIL_H__
 #define _KRAFT_UTIL_H__
 
+#include "kraft.pb.h"
+#include "kraft/macro.h"
+#include "kraft/type.h"
+
+namespace kraft {
+
+KRAFT_INLINE EntryMeta Entry2Meta(Entry const &ety) noexcept
+{
+  EntryMeta meta;
+  meta.index = ety.index();
+  meta.term = ety.term();
+  return meta;
+}
+
+} // namespace kraft
+
 #endif
