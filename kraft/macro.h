a0cc91b117139c5e5cea2735122683ab288ad57f
Conzxy <1967933844@qq.com>
2023-04-23 12:44:10 +0800

    util

--- kraft/macro.h
+++ kraft/macro.h
@@ -23,4 +23,30 @@
   class_ &operator=(class_ const &) = delete;                                  \
   class_(class_ const &) = delete
 
+#define KRAFT_FILE_NAME_LEN 256
+#define KRAFT_UNUSED(x_)    (void)(x_)
+
+#if defined(__GNUC__) || defined(__clang__)
+#  define KRAFT_TLS __thread
+#elif defined(_MSC_VER)
+#  define KRAFT_TLS __declspec(thread)
+#else
+#  ifdef CXX_STANDARD_11
+#    define KRAFT_TLS thread_local
+#  else
+#    define KRAFT_TLS
+#  endif
+#endif
+
+#define DEFINE_BOOLEAN_ENUM_CLASS(enum_name__)                                 \
+  enum class enum_name__ {                                                     \
+    ON = 1,                                                                    \
+    OFF = 0,                                                                   \
+  };
+
+#define KRAFT_ENTRY_LOG_EXT           ".kraftlog"
+#define KRAFT_META_LOG_EXT            ".kraftmeta"
+#define KRAFT_META_LOG_NAME           "kraftmeta"
+#define KRAFT_LOGGING_LOG_NAME_SUFFIX "-logging.kraftlog"
+
 #endif
--- kraft/type.h
+++ kraft/type.h
@@ -13,7 +13,20 @@ template <typename K, typename V, typename Hash = std::hash<K>,
 using HashMap = std::unordered_map<K, V, Hash, Pred, Alloc>;
 
 using u64 = uint64_t;
+using u32 = uint32_t;
 using u8 = uint8_t;
+using ull = unsigned long long;
+
+using size_header_t = u32;
+
+// Don't use macro to represent const variable
+// unnamed enum is a good replacement.
+enum : u64 {
+  INVALID_TERM = (u64)-1,
+  INVALID_INDEX = (u64)-1,
+  INVALID_VOTED_FOR = (u64)-1,
+  INVALID_ID = (u64)-1,
+};
 
 // The metadata describe the log entry
 //
